d196542f62399df77c34611c0251dfa1965ba802
nisha <nisha@pune-desktop>
2023-04-19 20:13:38 +0530

    Excel handling completed

--- utilities/file-handling/src/main/java/io/nishadc/automationtestingframework/filehandling/ExcelFileHandling.java
+++ utilities/file-handling/src/main/java/io/nishadc/automationtestingframework/filehandling/ExcelFileHandling.java
@@ -1,8 +1,16 @@
 package io.nishadc.automationtestingframework.filehandling;
 
-//logger
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.Logger;
-//Excel Apache POI
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,85 +21,114 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import io.nishadc.automationtestingframework.filehandling.exceptions.ExcelFileHandlingException;
 import io.nishadc.automationtestingframework.logging.LoggerFactory;
 
-//custom exceptions
-//java util
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Iterator;
-//java io
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-/**
-* <b>Class Name</b>: ExcelFileHandling<br>
-* <b>Description</b>: Provides an interface to read with Excel files.<br>
-* @author Nishad Chayanakhawa &lt;nishad.chayanakhawa@gmail.com&gt;
-*/
 public class ExcelFileHandling {
-	//logger
 	private static final Logger logger = LoggerFactory.create(ExcelFileHandling.class);
-
+	
 	private ExcelFileHandling() {
-		//Do Nothing. Object will never be instantiated.
+		
 	}
 	
-	/**
-	 * <b>Method Name</b>: getExcelWorkbook<br>
-	 * <b>Description</b>: Returns excel workbook.<br>
-	 * <b>Returns</b>: Excel workbook as {@link org.apache.poi.ss.usermodel.Workbook Workbook}<br>
-	 * @since v1.0.0
-	 * @param fullWorkbookPath Full Excel workbook path as {@link java.lang.String String}
-	 * @return Excel workbook as {@link org.apache.poi.ss.usermodel.Workbook Workbook}
-	 * @throws ExcelFileHandlingException in case of issues reading from excel file
-	 */
-	private static Workbook getExcelWorkbook(String fullWorkbookPath) throws ExcelFileHandlingException {
-		//open workbook file stream
-		File workbookFile = new File(fullWorkbookPath);
-		FileInputStream workbookFileInputStream = null;
+	private static ExcelFileType getWorkbookType(String fullWorkbookPath) throws ExcelFileHandlingException {
 		try {
-			workbookFileInputStream = new FileInputStream(workbookFile);
+			//parse workbook type from file extension name. Currently restricted to .xls and .xlsx
+			return ExcelFileType
+					.valueOf(fullWorkbookPath.substring(fullWorkbookPath.lastIndexOf(".") + 1).toUpperCase());
+		} catch (IllegalArgumentException e) {
+			//in case of file extension other than .xls and .xlsx
+			throw (ExcelFileHandlingException) new ExcelFileHandlingException(
+					"Invalid file type. Only .xls and .xlsx format is supported.").initCause(e);
+		}
+	}
+	
+	private static List<Map<String, Object>> getExcelSheetContent
+		(FileInputStream workbookFileInputStream,ExcelFileType workbookType,String worksheetName) 
+				throws IOException, ExcelFileHandlingException {
+		List<Map<String, Object>> excelData=null;
+		if(workbookType==ExcelFileType.XLS) {
+			try(Workbook workbook=new HSSFWorkbook(workbookFileInputStream)) {
+				excelData=getExcelSheetContent(workbook,worksheetName);
+			}
+		} else {
+			try(Workbook workbook=new XSSFWorkbook(workbookFileInputStream)) {
+				excelData=getExcelSheetContent(workbook,worksheetName);
+			}
+		}
+		return excelData;
+	}
+	
+	public static List<Map<String, Object>> getExcelSheetContent(String fullWorkbookPath,String worksheetName) throws IOException, ExcelFileHandlingException {
+		ExcelFileHandling.logger.debug("Reading Excel from {} workbook ans sheet {}", fullWorkbookPath,worksheetName);
+		
+		File workbookFile = new File(fullWorkbookPath);
+		try(FileInputStream workbookFileInputStream = new FileInputStream(workbookFile)) {
+			ExcelFileType workbookType =ExcelFileHandling.getWorkbookType(fullWorkbookPath);
+			return ExcelFileHandling.getExcelSheetContent(workbookFileInputStream, workbookType, worksheetName);
 		} catch (FileNotFoundException e) {
-			//In case file is not found
+			ExcelFileHandling.logger.error("Excel file [{}] doesn't exist.", fullWorkbookPath,e);
 			throw (ExcelFileHandlingException) new ExcelFileHandlingException(
 					String.format("Excel file [%s] doesn't exist.", fullWorkbookPath)).initCause(e);
-		}
-		ExcelFileType workbookType = null;
-		try {
-			try {
-				//parse workbook type from file extension name. Currently restricted to .xls and .xlsx
-				workbookType = ExcelFileType
-						.valueOf(fullWorkbookPath.substring(fullWorkbookPath.lastIndexOf(".") + 1).toUpperCase());
-			} catch (IllegalArgumentException e) {
-				//in case of file extension other than .xls and .xlsx
-				workbookFileInputStream.close();
-				throw (ExcelFileHandlingException) new ExcelFileHandlingException(
-						"Invalid file type. Only .xls and .xlsx format is supported.").initCause(e);
-			}
-			
-			//Assign workbook based on xls and xlsx type
-			Workbook workbook=null;
-			switch (workbookType) {
-			case XLS:
-				workbook=new HSSFWorkbook(workbookFileInputStream);
-				workbookFileInputStream.close();
-				break;
-			case XLSX:
-				workbook=new XSSFWorkbook(workbookFileInputStream);
-				workbookFileInputStream.close();
-				break;
-			}
-			//return workbook
-			return workbook;
-		} catch(IllegalArgumentException | IOException e) {
-			//in case excel file is corrupted or any other issue reading excel
-			ExcelFileHandling.logger.error("Corrupted excel file: " + e.getMessage(),e);
+		} catch(IllegalArgumentException e) {
+			ExcelFileHandling.logger.error("Corrupted excel file: {}",e.getMessage(),e);
 			throw (ExcelFileHandlingException) new ExcelFileHandlingException(
 					"Error reading excel file: " + e.getMessage()).initCause(e);
 		}
+	}
+	
+	private static List<Map<String, Object>> getExcelSheetContent(Workbook workbook,String worksheetName) throws ExcelFileHandlingException {
+		Sheet sheet=workbook.getSheet(worksheetName);
+		if(sheet==null) {
+			ExcelFileHandling.logger.error("Cannot find sheet {}", worksheetName);
+			throw new ExcelFileHandlingException(String.format("Sheet %s dpoesn't exist.", worksheetName));
+		}
+		
+		List<Map<String, Object>> rows = new ArrayList<>();
+		Iterator<Row> rowIterator = sheet.iterator();
+      String[] headers = {""};
+      //process all rows
+      while (rowIterator.hasNext()) {
+          Row currentRow = rowIterator.next();
+          if (currentRow.getRowNum() == 0) {
+              headers = getHeaders(currentRow);
+              continue;
+          }
+          Map<String, Object> rowMap = new HashMap<>();
+          for (int i = 0; i < headers.length; i++) {
+              Cell currentCell = currentRow.getCell(i);
+              rowMap.put(headers[i],ExcelFileHandling.getCellValue(currentCell) );
+          }
+          rows.add(rowMap);
+      }
+      return rows;
+	}
+	
+	private static Object getCellValue(Cell cell) {
+		if (cell == null) {
+            return "";
+        } else {
+        	//process each type of excel cell
+            switch (cell.getCellType()) {
+                case STRING:
+                    return cell.getStringCellValue();
+                case NUMERIC:
+                    return cell.getNumericCellValue();
+                case BOOLEAN:
+                    return cell.getBooleanCellValue();
+                //process formula cell type
+                case FORMULA:
+                	switch(cell.getCachedFormulaResultType()) {
+                	case STRING:
+                		return cell.getStringCellValue();
+                	case NUMERIC:
+                        return cell.getNumericCellValue();
+                    case BOOLEAN:
+                        return cell.getBooleanCellValue();
+						default:
+							return "";
+                	}
+                default:
+                    return "";
+            }
+        }
 	}
 	
 	/**
@@ -111,85 +148,4 @@ public class ExcelFileHandling {
       }
       return headers.toArray(new String[headers.size()]);
   }
-	
-	/**
-	 * <b>Method Name</b>: getExcelSheetContent<br>
-	 * <b>Description</b>: Get excel sheet contents as List&lt;Map&lt;String, Object&gt;&gt;<br>
-	 * <b>Returns</b>: List&lt;Map&lt;String, Object&gt;&gt; <br>
-	 * @since v1.0.0
-	 * @param fullWorkbookPath Full Excel workbook path as {@link java.lang.String String}
-	 * @param worksheetName as {@link java.lang.String String}
-	 * @return Excel sheet contents as List&lt;Map&lt;String, Object&gt;&gt;
-	 * @throws ExcelFileHandlingException in case of issues in reading file.
-	 */
-	public static List<Map<String, Object>> getExcelSheetContent(String fullWorkbookPath,String worksheetName) throws ExcelFileHandlingException {
-		ExcelFileHandling.logger.debug("Reading Excel from {} workbook ans sheet {}", fullWorkbookPath,worksheetName);
-		Workbook workbook=ExcelFileHandling.getExcelWorkbook(fullWorkbookPath);
-		Sheet sheet=workbook.getSheet(worksheetName);
-		if(sheet==null) {
-			ExcelFileHandling.logger.error("In workbook {}, cannot find sheet {}", fullWorkbookPath,worksheetName);
-			throw new ExcelFileHandlingException(String.format("Sheet %s dpoesn't exist in workbook %s", worksheetName,fullWorkbookPath));
-		}
-		List<Map<String, Object>> rows = new ArrayList<>();
-		Iterator<Row> rowIterator = sheet.iterator();
-      String[] headers = null;
-      //process all rows
-      while (rowIterator.hasNext()) {
-          Row currentRow = rowIterator.next();
-          if (currentRow.getRowNum() == 0) {
-              headers = getHeaders(currentRow);
-              continue;
-          }
-          Map<String, Object> rowMap = new HashMap<>();
-          for (int i = 0; i < headers.length; i++) {
-              Cell currentCell = currentRow.getCell(i);
-              if (currentCell == null) {
-                  rowMap.put(headers[i], "");
-              } else {
-              	//process each type of excel cell
-                  switch (currentCell.getCellType()) {
-                      case STRING:
-                          rowMap.put(headers[i], currentCell.getStringCellValue());
-                          break;
-                      case NUMERIC:
-                          rowMap.put(headers[i], currentCell.getNumericCellValue());
-                          break;
-                      case BOOLEAN:
-                          rowMap.put(headers[i], currentCell.getBooleanCellValue());
-                          break;
-                      //process formula cell type
-                      case FORMULA:
-                      	switch(currentCell.getCachedFormulaResultType()) {
-                      	case STRING:
-                      		rowMap.put(headers[i], currentCell.getStringCellValue());
-                              break;
-                      	case NUMERIC:
-                              rowMap.put(headers[i], currentCell.getNumericCellValue());
-                              break;
-                          case BOOLEAN:
-                              rowMap.put(headers[i], currentCell.getBooleanCellValue());
-                              break;
-							default:
-								rowMap.put(headers[i], "");
-	                            break;
-                      	}
-                      	break;
-                      default:
-                          rowMap.put(headers[i], "");
-                          break;
-                  }
-              }
-          }
-          try {
-          	//close workbook
-				workbook.close();
-			} catch (IOException e) {
-				ExcelFileHandling.logger.error("Error closing excel file: " + e.getMessage(),e);
-				throw (ExcelFileHandlingException) new ExcelFileHandlingException(
-						"Error closing excel file: " + e.getMessage()).initCause(e);
-			}
-          rows.add(rowMap);
-      }
-      return rows;
-	}
 }
--- utilities/file-handling/src/test/java/io/nishadc/automationtestingframework/filehandling/unittests/ExcelFileHandlingTests.java
+++ utilities/file-handling/src/test/java/io/nishadc/automationtestingframework/filehandling/unittests/ExcelFileHandlingTests.java
@@ -1,5 +1,6 @@
 package io.nishadc.automationtestingframework.filehandling.unittests;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -11,7 +12,7 @@ import io.nishadc.automationtestingframework.filehandling.exceptions.ExcelFileHa
 
 public class ExcelFileHandlingTests {
 	@Test
-	public void getExcelSheetContent_invalidFileType_test() {
+	public void getExcelSheetContent_invalidFileType_test() throws IOException {
 		try {
 			ExcelFileHandling.getExcelSheetContent("./src/test/resources/TestJsonFile.json", null);
 		} catch (ExcelFileHandlingException e) {
@@ -20,7 +21,7 @@ public class ExcelFileHandlingTests {
 	}
 	
 	@Test
-	public void getExcelSheetContent_invalidFile_test() {
+	public void getExcelSheetContent_invalidFile_test() throws IOException {
 		try {
 			ExcelFileHandling.getExcelSheetContent("./src/test/resources/Invalid.xls", null);
 		} catch (ExcelFileHandlingException e) {
@@ -29,7 +30,7 @@ public class ExcelFileHandlingTests {
 	}
 
 	@Test
-	public void getExcelSheetContent_corruptFile_test() {
+	public void getExcelSheetContent_corruptFile_test() throws IOException {
 		try {
 			ExcelFileHandling.getExcelSheetContent("./src/test/resources/CorruptedExcel.xls", null);
 		} catch (ExcelFileHandlingException e) {
@@ -38,7 +39,7 @@ public class ExcelFileHandlingTests {
 	}
 
 	@Test
-	public void getExcelSheetContent_invalidSheet_test() {
+	public void getExcelSheetContent_invalidSheet_test() throws IOException {
 		try {
 			ExcelFileHandling.getExcelSheetContent("./src/test/resources/TestData.xls", "Invalid Sheet");
 		} catch (ExcelFileHandlingException e) {
@@ -47,48 +48,58 @@ public class ExcelFileHandlingTests {
 	}
 
 	@Test
-	public void getExcelSheetContent_xls_test() throws ExcelFileHandlingException {
+	public void getExcelSheetContent_xls_test() throws ExcelFileHandlingException,IOException {
 		List<Map<String,Object>> testData=ExcelFileHandling.getExcelSheetContent("./src/test/resources/TestData.xls", "Data");
 		Assertions.assertThat(testData).hasSize(4);
-		Assertions.assertThat(testData.get(0).get("Parameter1")).isEqualTo("Value1");
-		Assertions.assertThat(testData.get(0).get("Parameter2")).isEqualTo("Value2");
-		Assertions.assertThat(testData.get(0).get("Parameter3")).isEqualTo("Value3");
-		Assertions.assertThat(testData.get(1).get("Parameter1")).isEqualTo(1.0);
-		Assertions.assertThat(testData.get(1).get("Parameter2")).isEqualTo(2.0);
-		Assertions.assertThat(testData.get(1).get("Parameter3")).isEqualTo(3.0);
-		Assertions.assertThat(testData.get(2).get("Parameter1")).isEqualTo(true);
-		Assertions.assertThat(testData.get(2).get("Parameter2")).isEqualTo(false);
-		Assertions.assertThat(testData.get(2).get("Parameter3")).isEqualTo(true);
-		Assertions.assertThat(testData.get(3).get("Parameter1")).isEqualTo(1.1);
-		Assertions.assertThat(testData.get(3).get("Parameter2")).isEqualTo(2.22);
-		Assertions.assertThat(testData.get(3).get("Parameter3")).isEqualTo(3.333);
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter1", "Value1");
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter2", "Value2");
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter3", "Value3");
+		
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter1", 1.0);
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter2", 2.0);
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter3", 3.0);
+		
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter1", true);
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter2", false);
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter3", true);
+		
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter1", 1.1);
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter2", 2.22);
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter3", 3.333);
 	}
 	
 	@Test
-	public void getExcelSheetContent_xlsx_test() throws ExcelFileHandlingException {
+	public void getExcelSheetContent_xlsx_test() throws ExcelFileHandlingException,IOException {
 		List<Map<String,Object>> testData=ExcelFileHandling.getExcelSheetContent("./src/test/resources/TestData.xlsx", "Data");
 		Assertions.assertThat(testData).hasSize(8);
-		Assertions.assertThat(testData.get(0).get("Parameter1")).isEqualTo("Value1");
-		Assertions.assertThat(testData.get(0).get("Parameter2")).isEqualTo("Value2");
-		Assertions.assertThat(testData.get(0).get("Parameter3")).isEqualTo("Value3");
-		Assertions.assertThat(testData.get(1).get("Parameter1")).isEqualTo(1.0);
-		Assertions.assertThat(testData.get(1).get("Parameter2")).isEqualTo(2.0);
-		Assertions.assertThat(testData.get(1).get("Parameter3")).isEqualTo(3.0);
-		Assertions.assertThat(testData.get(2).get("Parameter1")).isEqualTo(true);
-		Assertions.assertThat(testData.get(2).get("Parameter2")).isEqualTo(false);
-		Assertions.assertThat(testData.get(2).get("Parameter3")).isEqualTo(true);
-		Assertions.assertThat(testData.get(3).get("Parameter1")).isEqualTo(1.1);
-		Assertions.assertThat(testData.get(3).get("Parameter2")).isEqualTo(2.22);
-		Assertions.assertThat(testData.get(3).get("Parameter3")).isEqualTo(3.333);
-		Assertions.assertThat(testData.get(4).get("Parameter1")).isEqualTo(true);
-		Assertions.assertThat(testData.get(4).get("Parameter2")).isEqualTo("Value1Value2");
-		Assertions.assertThat(testData.get(4).get("Parameter3")).isEqualTo(6.653);
-		Assertions.assertThat(testData.get(5).get("Parameter1")).isEqualTo("");
-		Assertions.assertThat(testData.get(5).get("Parameter2")).isEqualTo("");
-		Assertions.assertThat(testData.get(5).get("Parameter3")).isEqualTo("");
-		Assertions.assertThat(testData.get(6).get("Parameter1")).isEqualTo("");
-		Assertions.assertThat(testData.get(6).get("Parameter2")).isEqualTo("Not Null");
-		Assertions.assertThat(testData.get(6).get("Parameter3")).isEqualTo("");
-		Assertions.assertThat(testData.get(7).get("Parameter1")).isEqualTo(" ");
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter1", "Value1");
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter2", "Value2");
+		Assertions.assertThat(testData.get(0)).containsEntry("Parameter3", "Value3");
+		
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter1", 1.0);
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter2", 2.0);
+		Assertions.assertThat(testData.get(1)).containsEntry("Parameter3", 3.0);
+		
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter1", true);
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter2", false);
+		Assertions.assertThat(testData.get(2)).containsEntry("Parameter3", true);
+		
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter1", 1.1);
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter2", 2.22);
+		Assertions.assertThat(testData.get(3)).containsEntry("Parameter3", 3.333);
+		
+		Assertions.assertThat(testData.get(4)).containsEntry("Parameter1", true);
+		Assertions.assertThat(testData.get(4)).containsEntry("Parameter2", "Value1Value2");
+		Assertions.assertThat(testData.get(4)).containsEntry("Parameter3", 6.653);
+		
+		Assertions.assertThat(testData.get(5)).containsEntry("Parameter1", "");
+		Assertions.assertThat(testData.get(5)).containsEntry("Parameter2", "");
+		Assertions.assertThat(testData.get(5)).containsEntry("Parameter3", "");
+		
+		Assertions.assertThat(testData.get(6)).containsEntry("Parameter1", "");
+		Assertions.assertThat(testData.get(6)).containsEntry("Parameter2", "Not Null");
+		Assertions.assertThat(testData.get(6)).containsEntry("Parameter3", "");
+		
+		Assertions.assertThat(testData.get(7)).containsEntry("Parameter1", " ");
 	}
 }
