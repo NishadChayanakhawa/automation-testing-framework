23f5626742550c36626ff8927500a058e269e91e
nisha <nisha@pune-desktop>
2023-04-29 19:28:17 +0530

    SOAP API Completed

---
+++ testing-interface/soap-api/src/test/java/io/nishadc/automationtestingframework/testinginterface/soapapi/unittests/SOAPAPITestHelperTests.java
@@ -0,0 +1,38 @@
+package io.nishadc.automationtestingframework.testinginterface.soapapi.unittests;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.w3c.dom.Node;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+import io.nishadc.automationtestingframework.filehandling.FlatFileHandling;
+import io.nishadc.automationtestingframework.filehandling.exceptions.FlatFileHandlingException;
+import io.nishadc.automationtestingframework.testinginterface.soapapi.SOAPAPITestHelper;
+
+public class SOAPAPITestHelperTests {
+	@SuppressWarnings("serial")
+	private static final Map<String,String> headersMap=new HashMap<String, String>() {{
+	    put("content-type", "text/xml; charset=utf-8");
+	}};
+	@Test
+	public void validTest() throws XPathExpressionException, SOAPException, IOException, FlatFileHandlingException  {
+		String payload=FlatFileHandling.getFileContents("./src/test/resources/NumberToWords.xml");
+		SOAPMessage request=SOAPAPITestHelper.formRequest(headersMap, null, payload);
+		SOAPMessage response=SOAPAPITestHelper.getResponse(request, "https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+		Node node=SOAPAPITestHelper.query(response,"m","http://www.dataaccess.com/webservicesserver/", "//m:NumberToWordsResult").item(0);
+		Assertions.assertThat(node.getTextContent()).isEqualTo("five hundred ");
+	}
+	
+	@Test
+	public void validTest1() throws FlatFileHandlingException, IOException, SOAPException {
+		String payload=FlatFileHandling.getFileContents("./src/test/resources/NumberToWords.xml");
+		SOAPMessage request=SOAPAPITestHelper.formRequest(null,"Random", payload);
+		SOAPAPITestHelper.getResponse(request, "https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+	}
+}
