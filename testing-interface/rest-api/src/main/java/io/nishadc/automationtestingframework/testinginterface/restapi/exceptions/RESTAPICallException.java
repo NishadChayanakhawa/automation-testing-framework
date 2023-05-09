580419f19e604bb8743bfd9dc753f62ffcff7aff
nisha <nisha@pune-desktop>
2023-05-09 10:45:29 +0530

    Nullable response quality issue fixed

---
+++ testing-interface/rest-api/src/main/java/io/nishadc/automationtestingframework/testinginterface/restapi/exceptions/RESTAPICallException.java
@@ -0,0 +1,12 @@
+package io.nishadc.automationtestingframework.testinginterface.restapi.exceptions;
+
+public class RESTAPICallException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public RESTAPICallException(String message) {
+		super(message);
+	}
+}
